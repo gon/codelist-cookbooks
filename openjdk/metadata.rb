32431f8829b30fa27ee8a80c85cf6257ecce5d91
Gon Zifroni <gon@labzero.com>
2014-03-18 18:34:29 -0700

    Fixed recipe name.

--- openjdk/metadata.rb
+++ openjdk/metadata.rb
@@ -1,11 +1,11 @@
-name              "java-devel"
+name              "openjdk"
 maintainer        "Gon Zifroni"
 maintainer_email  "gon@labzero.com"
 license           "MIT"
-description       "Installs java-devel"
+description       "Installs openjdk"
 version           "0.1.0"
 
-recipe "java-devel", "Installs Java Development"
+recipe "openjdk", "Installs Java Development"
 
 %w[ubuntu debian redhat centos scientific fedora suse amazon].each do |os|
   supports os
